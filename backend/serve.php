2b557b7dea7fac1d3d576521a1c11c229073fb8d
Bishoy Romany <7bishoyromany7@gmail.com>
2020-05-13 11:42:20 +0200

    DONE

---
+++ backend/serve.php
@@ -0,0 +1,159 @@
+<?php 
+    ini_set('max_execution_time', 3600);
+
+    function get_string_between($string, $start, $end){
+        $string = ' ' . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return '';
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
+
+    function dd($data){
+        echo "<pre>";
+            print_r($data);
+        echo "</pre>";
+        exit;
+    }
+
+    function toArray($data){
+        $returned = [];
+        foreach($data as $d){
+            $returned[] = json_decode($d);
+        }
+        return $returned;
+    }
+
+    function toJson($data){
+        $returned = [];
+        foreach($data as $d){
+            $returned[] = json_encode($d);
+        }
+        return $returned;
+    }
+
+
+/**
+ * get variables
+ */
+$url = isset($_GET['url']) && !empty($_GET['url']) ? filter_var($_GET['url'], FILTER_SANITIZE_STRING) : false;
+if(!$url){ echo "Make sure to Write The URL"; exit; }
+
+$columns = isset($_GET['columns']) && !empty($_GET['columns']) ? explode(',',filter_var($_GET['columns'], FILTER_SANITIZE_STRING)) : false;
+if(!$columns){ echo "Make sure to Write The Columsn"; exit; }
+$filterDuplicates = isset($_GET['filterDuplicates']) ? filter_var($_GET['filterDuplicates'], FILTER_SANITIZE_STRING) : false;
+$allInOneFile = isset($_GET['allInOneFile']) ? filter_var($_GET['allInOneFile'], FILTER_SANITIZE_STRING) : false;
+$fileName = isset($_GET['fileName']) && !empty($_GET['fileName']) ? filter_var($_GET['fileName'], FILTER_SANITIZE_STRING).'.csv' : 'fileName.csv';
+
+$cColumns = [];
+foreach($columns as $column){
+    $cc = explode('-', $column);
+    $cColumns[] = [
+        'id' => $cc[0],
+        'name' => $cc[1],
+    ];
+}
+
+if(isset($_GET['serve'])){
+    // get urls
+    $data = file_get_contents($url);
+    $data = get_string_between($data, 'style="border:none;" frameborder="0"></iframe>', '</div>');
+    
+    // filter all urls and store them in files variable
+    $files = [];
+    $filesSub = explode("<a href=", $data);
+    foreach($filesSub as $file){
+        if(!empty(get_string_between($file, '"', '"'))){
+            $files[] = get_string_between($file, '"', '"');
+        }
+    }
+    
+    // clear memory
+    unset($data);
+    unset($filesSub);
+
+    // the final result
+    $finalCsv = [];
+
+    // loop the files to get the csv files
+    $x = 0;
+
+    foreach($files as $file){
+        $hash = $x.'.csv';
+        if(!file_exists(__DIR__.'/files/'.$hash)){
+            $csvData = file_get_contents($file);
+            file_put_contents(__DIR__.'/files/'.$hash, $csvData);
+        }
+
+        $file = fopen(__DIR__.'/files/'.$hash, "r");
+        $row = 0;
+        $headers = [];
+        
+        $subFinal = [];
+
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+            if($row > 0){
+                $ddd = [];
+                foreach($cColumns as $cc){
+                    $ddd[$cc['name']] = $data[$cc['id']];
+                }
+
+                if($allInOneFile){
+                    $finalCsv[] = $ddd;
+                }else{
+                    $subFinal[] = $ddd;
+                    dd("stop");
+                }
+            }else{
+                $num = count($data);
+                for ($c=0; $c < $num; $c++) {
+                    $headers[] = $data[$c];
+                } 
+            }
+            $row ++;
+        }
+
+        fclose($file);
+        
+        $x ++;
+    }
+
+    if($allInOneFile){
+        // Open a file in write mode ('w') 
+        $csvFile = fopen($fileName, 'w'); 
+        $finalCsv = toArray(array_unique(toJson($finalCsv)));
+        $header = [];
+        foreach($cColumns as $c){
+            $header[] = $c['name'];
+        }
+        fputcsv($csvFile, $header); 
+        foreach($finalCsv as $cs){
+            fputcsv($csvFile, (array)$cs); 
+        }
+        fclose($csvFile);
+
+        // download the file
+        if (file_exists($fileName)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename='.basename($fileName));
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($fileName));
+            ob_clean();
+            flush();
+            readfile($fileName);
+            exit;
+        }
+
+    }else{
+        $subFinal[] = $ddd;
+        dd("stop");
+    }
+
+}else{
+    echo "You are not allowd to view this page"; exit;
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>CSV Convertor Scraper</title>
+</head>
+<body>  
+    <br><br><br><br>
+    <div class="container">
+        <h1 class="text-center">Please Fill The Following Information</h1>
+        <form action="backend/serve.php" method="get" value='https://sellersjsons.com/sellersjson_csv.html'>
+            <div class="form-group">
+                <label for="url">Website URL</label>
+                <input type="text" name="url" id="url" class="form-control" value='https://sellersjsons.com/sellersjson_csv.html'>
+            </div>
+
+            <div class="form-group">
+                <label for="columns">Columns</label>
+                <textarea name="columns" id="columns" class="form-control"  cols="30" rows="5">2-domain,3-seller_type</textarea>
+            </div>
+
+            <label for="filterDuplicates">
+                Filter Duplicates 
+                <input type="checkbox" name="filterDuplicates" id="filterDuplicates" value="1" checked>
+            </label> <br>
+            <label for="allInOneFile">
+                All In One File
+                <input type="checkbox" name="allInOneFile" id="allInOneFile" value="1" checked>
+            </label> <br>
+            <div class="form-group">
+                <label for="fileName">File Name</label>
+                <input type="text" name="fileName" id="fileName" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary" name="serve">Start</button>
+        </form>
+    </div>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
